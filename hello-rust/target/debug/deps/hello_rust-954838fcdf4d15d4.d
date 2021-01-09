fb69aa9cbf0ad6526a9960aa82b0557b14dcec42
ulil <ulil@protonmail.com>
2021-01-09 14:15:58 +0100

    init

---
+++ hello-rust/target/debug/deps/hello_rust-954838fcdf4d15d4.d
@@ -0,0 +1,5 @@
+/home/nebula/Documents/GitHub-Ulil/rust/hello-rust/target/debug/deps/hello_rust-954838fcdf4d15d4: src/main.rs
+
+/home/nebula/Documents/GitHub-Ulil/rust/hello-rust/target/debug/deps/hello_rust-954838fcdf4d15d4.d: src/main.rs
+
+src/main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/home/nebula/Documents/GitHub-Ulil/rust/hello-rust/target/debug/hello-rust: /home/nebula/Documents/GitHub-Ulil/rust/hello-rust/src/main.rs
